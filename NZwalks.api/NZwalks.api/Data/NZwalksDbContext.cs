b7b7052aea747fce5d69ff9ae876204268d77771
mohd_bhati <mohd_bhati@persistent.com>
2022-09-08 16:55:51 +0530

    first

---
+++ NZwalks.api/NZwalks.api/Data/NZwalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZwalks.api.Models.Domain;
+
+namespace NZwalks.api.Data
+{
+    public class NZwalksDbContext: DbContext
+    {
+        public NZwalksDbContext(DbContextOptions<NZwalksDbContext> options):base(options)
+            { 
+        
+        }
+
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDificulty> WalkDificulty { get; set; }
+
+
+    }
+}
---
+++ NZwalks.api/NZwalks.api/Models/Domain/Walk.cs
@@ -0,0 +1,18 @@
+﻿namespace NZwalks.api.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        public Guid WalkDificultyId { get; set; }
+
+        //Navigation
+
+        public Region Region { get; set; }
+        public WalkDificulty WalkDificulty { get; set; }
+    }
+}
